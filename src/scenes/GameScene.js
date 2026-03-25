af3bb93d1c71408f528c5bf60069b65221e41105
Ahmed-islam03 <ai306306@gmail.com>
2026-03-25 23:54:03 +0500

    error fixed

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -503,7 +503,7 @@ export default class GameScene extends Phaser.Scene {
                     ease: 'Sine.easeInOut'
                 });
             } else {
-                Text.setText('GO!');
+                countText.setText('GO!');
                 countText.setScale(1);
                 this.tweens.add({
                     targets: countText,
